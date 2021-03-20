1b698fdb1d66a8155ee14626550ae1f8e783d6ef
DELL <User@WIN-U2KHTL0HNOE.sl-ira.local>
2021-03-20 14:47:18 +0200

    IsPrime Check

---
+++ isPrime.py
@@ -0,0 +1,9 @@
+def isPrime(n):
+    if n == 1:
+        return False
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+        d += 2
+    return d * d > n
